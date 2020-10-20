ee06199e5d9f8ee3b6fee28250bd37256c9d89f2
pranav.pandey@successive.tech <pranav.pandey@successive.tech>
2020-10-20 18:44:01 +0530

    Docs: Updated Armstrong.js

--- Programs/armstrong.js
+++ Programs/armstrong.js
@@ -3,9 +3,10 @@
 function armStrong(num){
       temp = num;
       sum = 0;
+      d = num.toString().length;
       while(num!=0){
             r=num%10;
-            sum = sum+(r*r*r);
+            sum = sum+Math.pow(r,d);
             num= parseInt(num/10);
 
       }
@@ -19,4 +20,4 @@ function armStrong(num){
             console.log("This is not armstong number");
       }
 }
-armStrong(153);
+armStrong(1634);
