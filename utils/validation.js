fb3bc7940ccb9f3e1c8de76d4a7d6d0619e86626
pranav.pandey@successive.tech <pranav.pandey@successive.tech>
2020-10-21 14:23:19 +0530

    Docs: Updated validation.js

--- utils/validation.js
+++ utils/validation.js
@@ -1,52 +1,71 @@
-let index = 0
 let users = [
-      { traineeEmail: 'trainee1successive.tech'  ,
-       reviewerEmail: 'reviewer1@successive.tech'},
-      /*{ mentorEmail: 'mentor@successive'},
-      { leadEmail: 'teamleadsuccessive.tech'},
-      { rmEmail: 'rm'}*/
-      ]
- 
-      
-function validateEmail(users){
-      let reg =/^\w+([-+.']\w+)*@\w+([-.']\w+)*\.\w+([-.]\w+)*$/
-      if (reg.test(users)){
-            return true;
-            
+      {
+            traineeEmail: "prnv@successive.tech",
+            reviewerEmail: "pandey@successive.tech"
+      },
+      {
+            traineeEmail: "prnv@successive.tech*1",
+            reviewerEmail: "pandey@successive.tech"
+      },
+      {
+            traineeEmail: "prnv@successive.tech",
+            reviewerEmail: "pandey@successive.tech"
+      },
+      {
+            traineeEmail: "prnv@successive.tech",
+            reviewerEmail: "pandey@successive.tech"
+      },
+      {
+            traineeEmail: "prnv@successive.te/",
+            reviewerEmail: "pandey@successive.tech//*"
+      },
+      {
+            traineeEmail: "prnv@successive.te/*",
+            reviewerEmail: "pandey@successive.t"
+      },
+      {
+            traineeEmail: "prnv@successive.tech",
+            reviewerEmail: "pandey@successive.tech"
+      },
+      {
+            traineeEmail: "prnv@successive.tech98",
+            reviewerEmail: "pandey@successive.tech"
       }
-      else {
-            return false;
+];
+
+//Will check her formating of emails
+let checkemail = /^[a-zA-Z0-9._-]+@[successive]+\.[tech]{2,4}$/;
+
+let valid_users=[];
+let invalid_users=[];
+
+// function for validate the email.
+function validateEmail(email){
+      return checkemail.test(String(email).toLowerCase());
+}
+
+//function for validate the users
+function validateUsers(users)
+{   
+    users.forEach((users)=>{
+    const {traineeEmail,reviewerEmail} = users;
+    if(validateEmail(traineeEmail) && validateEmail(reviewerEmail)){
+        valid_users.push("("+traineeEmail+", "+reviewerEmail+") ");
       }
+      else{
+          invalid_users.push("("+traineeEmail+", "+reviewerEmail+") ");
+      }
+});
+
+//It will findout the valid users
+let validlen=valid_users.length;
+console.log("Total valid users:",validlen);
+console.log("The users are: ",valid_users);
+
+//It will findout who are invalid users
+let invalidlen=invalid_users.length;
+console.log('Total invalid users',invalidlen);
+console.log('The users is: ',invalid_users);
 }
-//validateEmail('prnv@successive.tech');
 
-function validateUser(users){
-      
-        
-      valid=0;
-      inValid=0;
-      
-      
-      users.forEach(({traineeEmail, reviewerEmail})=> {
-            if(validateEmail(traineeEmail)){
-                  valid++;
-            }
-            else{
-                  inValid++;
-            }
-            
-      
-      // const isValidT = validateEmail(traineeEmail);
-      // const isValidR = validateEmail(reviewerEmail);
-
-
-      });
-
-     
-
-      
-}
-validateUser(users);
-console.log(valid);
-console.log(inValid);
-
+validateUsers(users);
