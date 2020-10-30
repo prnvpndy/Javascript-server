c896f655dfb2dddfa3f75a575bf7af354c441e30
pranav.pandey@successive.tech <pranav.pandey@successive.tech>
2020-10-30 23:41:21 +0530

    Work in Progress

---
+++ src/libs/validationHandler.ts
@@ -0,0 +1,20 @@
+export default (config) => (req, res, next) =>{
+      console.log("config",config);
+      console.log(req.query);
+      //console.log(req.body);
+      
+      next();
+}
+
+const error = [
+      {              
+            key: "skip",      
+            location: "query",      
+            errorMessage: "Skip is invalid"      
+      },      
+      {      
+            key: "limit",      
+            location: "query",
+            errorMessage: "Limit is invalid"
+      }      
+]
