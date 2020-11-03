01502c4e4aea7b745790e2aa2e937676cd37b077
pranav.pandey@successive.tech <pranav.pandey@successive.tech>
2020-11-03 18:53:15 +0530

    Authentication and Authorization

--- dist/controllers/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.traineeRouter = void 0;
-var trainee_1 = require("./trainee");
-Object.defineProperty(exports, "traineeRouter", { enumerable: true, get: function () { return trainee_1.traineeRouter; } });
-//# sourceMappingURL=index.js.map
